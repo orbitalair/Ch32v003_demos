2de9d0a993646b2dd17eff7fae589429d638b174
jmrobert <orbitalair@gmail.com>
2025-02-10 20:18:26 -0600

    Add main readme

---
+++ README.mk
@@ -0,0 +1,8 @@
+Ch32v003fun Demo Programs.
+
+1. A pcf8563 i2c real time clock chip demo.
+
+2. A 24LC256 eeprom chip demo.  This one shows how to use 2 byte addressing for i2c.
+Modified the i2c lib to support 2 byte addresses.
+
+
